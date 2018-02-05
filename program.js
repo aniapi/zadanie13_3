5df3ffaa221afcdce5c1919a5c4b10ddd7a39538
Anna Piwnik <a.piwnik@o2.pl>
2018-02-05 13:28:36 +0100

    Finish task

--- program.js
+++ program.js
@@ -1,21 +1,25 @@
 process.stdin.setEncoding('utf-8');
+
 process.stdin.on('readable', function() {
-    // tutaj treść tego co ma się wykonać w momencie odczytania wejścia.
     var input = process.stdin.read();
     if(input !== null) {
-        var instruction = input.toString().trim();
-        if (instruction === '/exit') {
+           var instruction = input.toString().trim();
+        switch (instruction) {
+            case '/exit':
             process.stdout.write('Quitting app!\n');
             process.exit();
-        } else if (instruction === '/version') {
-            process.stdout.write('Wersja to:' + process.versions.node + '\n' + 'Język systemowy to:' + process.env.node + '\n');
-        }
-
-        else {
+            break;
+         case '/version':
+            process.stdout.write('Wersja to: ' + process.versions.node + '\n');
+            break;
+         case '/language':
+            process.stdout.write('Język systemowy to: ' + process.env.LANG + '\n');
+            break; 
+         default:
             process.stderr.write('Wrong instruction!\n');
         }
     }
 });
 
-process.versions;
-process.env;
+
+
